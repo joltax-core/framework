bc5c7325b6e5d0458b4403c76c5211d7d89fdd8e
Mucahit BUDAK <190402909@ogr.gelisim.edu.tr>
2025-02-06 10:00:11 +0300

    Fixed a few definition errors.

--- bin/main.dart
+++ bin/main.dart
@@ -5,6 +5,7 @@ import 'package:joltax_framework/taycan/taycan_service.dart';
 import 'package:joltax_framework/taycan/taycan.dart' as taycan_cli;
 
 void main(List<String> arguments) async {
-  await TaycanService.runCommand(arguments, allCommands: taycan_cli.allCommands, menu: taycanMenu);
+  await TaycanService.runCommand(arguments,
+      allCommands: taycan_cli.allCommands, menu: taycanMenu);
   exit(0);
 }
